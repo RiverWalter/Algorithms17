113359e4767684ce07b13a913cdfd5dafa2e50f0
RiverWalter <hcduan@sdnu.edu.cn>
2019-08-14 11:08:29 +0800

    BTnQueens complete version

--- Algorithms17/src/CS/AlgorithmD_A/BackTracking/BTnQueens.cpp
+++ Algorithms17/src/CS/AlgorithmD_A/BackTracking/BTnQueens.cpp
@@ -3,13 +3,13 @@ using namespace std;
 static void BTnQueens(int row);
 static bool CheckPlacing(int row, int col);
 static void Output();
-static vector<int> p;
+static vector<int> Col;
 static int N;
 static bool Done;
 void BTnQueensCaller(int n)
 {
     N = n;
-    p.resize(N);
+    Col.resize(N);
     Done = false;
     BTnQueens(0);
     Output();
@@ -19,7 +19,7 @@ static void BTnQueens(int row)
     for (int col = 0; !Done && col < N; col++)
         if (CheckPlacing(row, col))
         {
-            p[row] = col;
+            Col[row] = col;
             if (row == N - 1)
                 Done = true;
             else
@@ -29,8 +29,8 @@ static void BTnQueens(int row)
 static bool CheckPlacing(int row, int col)
 {
     for (int r = 0; r < row; r++)
-        if (p[r] == col || 
-            abs(p[r] - col) == abs(r - row))
+        if (Col[r] == col || 
+            abs(Col[r] - col) == abs(r - row))
             return false;
     return true;
 }
@@ -38,6 +38,6 @@ static void Output()
 {
     printf("Placement for %d-Queens: \n", N);
     for (int row = 0; row < N; row++)
-        printf("(%d,%d)\n", row + 1, p[row] + 1);
+        printf("(%d,%d)\n", row + 1, Col[row] + 1);
 }
 
